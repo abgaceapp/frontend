25b185c003f79458a1dbf1a393a41dc3a9479f9d
hodletoshi <hodlegill@protonmail.com>
2022-03-09 13:49:14 -0500

    Initial commit

---
+++ frontend/static/js/index.js
@@ -0,0 +1,71 @@
+import Landing from "./views/landing.js";
+import Territory from "./views/territory.js";
+import Store from "./views/store.js";
+
+/*
+import Play from "./views/play.js";
+import Mint from "./views/mint.js";
+import Details from "./views/details.js";
+import HowToPlay from "./views/instructions.js";
+import Faq from "./views/faq.js";
+import Rewards from "./views/rewards.js";
+import Attributes from "./views/attributes.js";
+*/
+
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
+const getParams = match => {
+    const values = match.result.slice(1);
+    const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+    return Object.fromEntries(keys.map((key, i) => {
+        return [key, values[i]];
+    }));
+};
+
+const navigateTo = url => {
+    history.pushState(null, null, url);
+    router();
+};
+
+const router = async () => {
+    const routes = [
+        { path: "/", view: Landing },
+        { path: "/territory", view: Territory },
+        { path: "/store", view: Store },
+    ];
+
+    // Test each route for potential match
+    const potentialMatches = routes.map(route => {
+        return {
+            route: route,
+            result: location.pathname.match(pathToRegex(route.path))
+        };
+    });
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            result: [location.pathname]
+        };
+    }
+
+    const view = new match.route.view(getParams(match));
+
+    document.querySelector("#app").innerHTML = await view.getHtml();
+};
+
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+
+    router();
+});
---
+++ frontend/static/js/views/store.js
@@ -0,0 +1,30 @@
+import AbstractView from "./AbstractView.js";
+
+export default class extends AbstractView {
+    constructor(params) {
+        super(params);
+        this.setTitle("Home");
+    }
+
+    async getHtml() {
+      return `
+        <div class = "store-top">
+          LCBO #15 <span class="dark-blue">&nbsp;Store Overview</span>
+        </div>
+        <div class="home-row">
+          <div class="leader-widget">
+            <h1>Live Leaderboard ($ Closed)</h1>
+            <h2>May 20 - May 27</h2>
+          </div>
+          <div class="wc-widget">
+            <h1>% of White Claw</h1>
+            <h2>May 20 - May 27</h2>
+          </div>
+          <div class="seltzer-widget">
+            <h1>Seltzer Mkt Share</h1>
+            <h2>May 20 - May 27</h2>
+          </div>
+        </div>
+      `;
+    }
+}
