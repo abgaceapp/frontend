3dc04710d1dbe595cb49077ce49ac2afa56328d8
hodletoshi <hodlegill@protonmail.com>
2022-04-26 15:10:23 -0400

    change White Claw Tea to WC Tea

--- frontend/static/js/views/territory.js
+++ frontend/static/js/views/territory.js
@@ -314,7 +314,7 @@ export default class extends AbstractView {
             <h1 class="detail-head" style="padding-top: 10px;">Seltzer<span class="detail-right" id="mktshare-seltz"></span><br><span style="font-size: 15px;">Market Share</span></h1>
             <h1 class="detail-head">Tea<span class="detail-right" id="mktshare-tea"></span><br><span style="font-size: 15px;">Market Share</span></h1>
             <h1 class="detail-head">White Claw<span class="detail-right" id="mktshare-wc"></span><br><span style="font-size: 15px;">% of Sales</span></h1>
-            <h1 class="detail-head">White Claw Tea<span class="detail-right" id="mktshare-wc-tea"></span><br><span style="font-size: 15px;">% of Sales</span></h1>
+            <h1 class="detail-head">WC Tea<span class="detail-right" id="mktshare-wc-tea"></span><br><span style="font-size: 15px;">% of Sales</span></h1>
             <h1 class="detail-head" style="margin-bottom: 10px;">Twisted Tea<span class="detail-right" id="mktshare-twisted"></span><br><span style="font-size: 15px;">% of Sales</span></h1>
           </div>
           <div class="table-widget">
